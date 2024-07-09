d967402921fab646101e8157396967756c4570e5
Bibidi <byungdeuk.kim@gmail.com>
2024-07-10 01:37:36 +0900

    Add menu toggle button for mobile

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,21 @@
+'use client'
+
 import Link from "next/link"
+import { useState } from "react"
 import "./navbar.css"
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <nav className="navbar">
       <h1 className="navbar-title"><Link href="/" className="navbar-title-link">Logo</Link></h1>
-      <ul className="navbar-nav">
+      <button className="navbar-toggler" onClick={toggleNavbar}>☰</button>
+      <ul className={`navbar-nav navbar-collapse ${isOpen ? "open" : ""}`}>
         <li className="nav-item"><Link href="/" className="nav-item-link">Home</Link></li>
         <li className="nav-item"><Link href="/HTML" className="nav-item-link">HTML</Link></li>
         <li className="nav-item"><Link href="/CSS" className="nav-item-link">CSS</Link></li >
