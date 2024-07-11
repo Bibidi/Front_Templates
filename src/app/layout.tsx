48e8a4df7a3c99171095179443a7a2166669976d
Bibidi <byungdeuk.kim@gmail.com>
2024-07-12 05:34:32 +0900

    Chage folder naming convention

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import Footer from "@/components/footer/Footer";
-import Header from "@/components/header/Header";
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
